e453cf12b0d155142bcdf94e2ade52eb683fae37
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2023-02-14 17:21:30 -0300

    Adicionar mapa em AboutUs

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,4 @@
+import { MdLocationOn } from 'react-icons/md'
 
 const About = () => {
     return (
@@ -13,7 +14,34 @@ const About = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nihil perspiciatis perferendis? Sequi repellendus saepe perferendis. Vel assumenda sequi expedita dicta perferendis. Quisquam sunt odit praesentium aut nemo at aperiam.
                 </p>
                 <hr/>
-                <div className="store-location"></div>
+                <div className="store-location">
+                    <h2> Visite nossa loja </h2>
+                    <div className="location">
+                        <div className="iframe">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11923.864085496709!2d-46.64516638760085!3d-23.544926705666168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5853d96a1441%3A0xebba5cad95ff31bb!2sZona%20Central%20de%20S%C3%A3o%20Paulo%20-%20Rep%C3%BAblica%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001037-010!5e0!3m2!1spt-PT!2sbr!4v1676403573569!5m2!1spt-PT!2sbr" width="400" height="300" style={{border: 0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                        <div className='address'>
+                            <span className="text-address">
+                                <MdLocationOn/>
+                                Rua Example, nº 10 - São Paulo/SP - CEP 01001-001
+                            </span>
+                            <div className='rotas'>
+                                <span>
+                                    <a href="#">
+                                        <img src="images/icons/waze-icon.png" alt="Waze" className='waze'/>
+                                    </a>
+                                    Veja no Waze
+                                </span>
+                                <span>
+                                    <a href="#">
+                                        <img src="images/icons/google-maps-icon.png" alt="Google Maps" className='google-maps'/>
+                                    </a>
+                                    Veja no Google Maps
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
